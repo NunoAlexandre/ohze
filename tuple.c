a4f954fcb2d4a29728790cbfb46362bc1766e5f8
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2014-09-22 21:05:46 +0100

    Tuple implemented.

--- tuple.c
+++ tuple.c
@@ -1,19 +1,39 @@
 //
 //  tuple.c
-//  SD_2014
+//  sd15project
 //
-//  Created by Bruno Mata on 22/09/14.
-//
+//  Created by Nuno Alexandre on 22/09/14.
+//  Copyright (c) 2014 Nuno Alexandre. All rights reserved.
 //
 
-#include "tuple.h"
 #include <stdlib.h>
+#include <string.h>
+#include "tuple.h"
 
+struct tuple_t *tuple_create(int tuple_dim) {
+    struct tuple_t * newTuple = (struct tuple_t*) malloc (sizeof(struct tuple_t));
+    
+    newTuple->tuple_dimension = tuple_dim;
+    //acho que nao faz sentido reservar memoria...
+    newTuple->tuple = (char**) malloc (sizeof(char)*tuple_dim);
+    
+    return newTuple;
+}
 
-/* Função que destrói um bloco de dados e liberta toda a memoria.
- */
-void tuple_destroy(struct tuple_t *tuple){
-    free(&(tuple->tuple_dimension));
+struct tuple_t *tuple_create2(int tuple_dim, char **tuple) {
+    struct tuple_t * newTuple = (struct tuple_t*) malloc (sizeof(struct tuple_t));
+    newTuple->tuple_dimension = tuple_dim;
+    memcpy( newTuple->tuple, tuple, sizeof(char) * sizeof(tuple));
+    
+    return newTuple;
+}
+
+void tuple_destroy(struct tuple_t *tuple) {
+    free( &(tuple->tuple_dimension));
     free(tuple->tuple);
     free(tuple);
+}
+
+struct tuple_t *tuple_dup (struct tuple_t *tuple) {
+    return tuple_create2(tuple->tuple_dimension, tuple->tuple);
 }
