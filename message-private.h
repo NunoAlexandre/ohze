a07934ae0f25c878cf4541c25bf9538aeeefd14e
Bruno Mata <bjcmata@gmail.com>
2014-10-17 17:44:24 +0100

    adicionar message-private definição de constantes de size!

---
+++ message-private.h
@@ -0,0 +1,19 @@
+//
+//  message-private.h
+//  SD15-Product
+//
+//  Created by Bruno Mata on 17/10/14.
+//  Copyright (c) 2014 Grupo SD015. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define OPCODE_SIZE 2
+#define C_TYPE_SIZE 2
+#define TUPLE_DIMENSION_SIZE 4
+#define TUPLE_ELEMENTSIZE_SIZE 4
+#define TIMESTAMP_SIZE 8
+#define ENTRY_DIMENSION_SIZE 4
+#define ENTRY_ELEMENTSIZE_SIZE 4
+#define RESULT_SIZE	4
+
