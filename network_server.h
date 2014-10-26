5c5cab242327a8d9473c3fbdca30f60d9f321c62
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2014-10-26 17:02:05 +0000

    Adicionado módulo network_server (.h e .c)

---
+++ network_server.h
@@ -0,0 +1,48 @@
+//
+//  network_server.h
+//  SD15-Product
+//
+//  Created by Nuno Alexandre on 26/10/14.
+//  Copyright (c) 2014 Nuno Alexandre. All rights reserved.
+//
+
+#ifndef SD15_Product_network_server_h
+#define SD15_Product_network_server_h
+
+#include "message-private.h"
+
+int socket();
+
+int bind();
+
+int listen();
+
+int accept();
+
+/* Esta função deve:
+ * - estabelecer a ligação com o servidor;
+ * - address_port é uma string no formato <hostname>:<port>
+ * (exemplo: 10.10.10.10:10000)
+ * - retornar toda a informacão necessária (e.g., descritor da
+ * socket) na estrutura server_t
+ */
+struct server_t *network_connect(const char *address_port);
+
+/* Esta função é a correspondente à send_receive do network_client.
+ * - Obter o descritor da ligação (socket) da estrutura server_t;
+ * - enviar a mensagem msg ao servidor;
+ * - receber uma resposta do servidor;
+ * - retornar a mensagem obtida como resposta ou NULL em caso
+ * de erro.
+ */
+struct message_t *network_receive_send(struct server_t *server,
+                                       struct message_t *msg);
+
+/* A funcao network_close() deve fechar a ligação estabelecida por
+ * network_connect(). Se network_connect() alocou memoria, a função
+ * deve libertar essa memoria.
+ */
+int network_close(struct server_t *server);
+
+
+#endif
