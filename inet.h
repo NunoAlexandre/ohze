d6cf3d5561d66eaa8b76826992310a6bac67eb06
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2014-10-26 18:46:03 +0000

    Include precios ao projecto - centro de includes para a comunicação pela rede.

---
+++ inet.h
@@ -0,0 +1,24 @@
+//
+//  inet.h
+//  SD15-Product
+//
+//  Created by Nuno Alexandre on 26/10/14.
+//  Copyright (c) 2014 Nuno Alexandre. All rights reserved.
+//
+
+#ifndef SD15_Product_inet_h
+#define SD15_Product_inet_h
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+//tamanho maximo da mensagem enviada pelo cliente
+#define MAX_MSG 2048
+
+#endif
--- network_server.c
+++ network_server.c
@@ -7,7 +7,7 @@
 //
 
 #include <stdio.h>
-
+#include "inet.h"
 
 int socket();
 
