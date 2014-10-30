d2221075dc9fc610980f81ca9e3b2ca922108ef0
Bruno Mata <bjcmata@gmail.com>
2014-10-30 20:45:12 +0000

    Define-utils.h

---
+++ define-utils.h
@@ -0,0 +1,19 @@
+//
+//  define-utils.h
+//  SD15-Product
+//
+//  Created by Bruno Mata on 30/10/14.
+//  Copyright (c) 2014 Grupo SD015. All rights reserved.
+//
+
+#ifndef SD15_Product_define_utils_h
+#define SD15_Product_define_utils_h
+
+#define TASK_FAILED -1
+#define TASK_SUCCEDED 0
+#define MAX_MSG 2048
+
+
+
+
+#endif
