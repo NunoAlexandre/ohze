f1d908e4a56b0e7cd1bb170d7cf227f9b867a035
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2014-10-31 18:32:03 +0000

    tuple_deserialize atualizado para passar de "*" para NULL.

--- tuple.c
+++ tuple.c
@@ -212,7 +212,7 @@ struct tuple_t *tuple_deserialize(char *buffer, int size) {
         char * elementValue = (char*) calloc(1, elementSize);
         
         memcpy(elementValue, (buffer+offset), elementSize);
-        tuple->tuple[i] = strdup(elementValue);
+        tuple->tuple[i] = elementValue == TUPLE_ELEM_NULL ? NULL : strdup(elementValue);
         free(elementValue);
         offset+=elementSize;
     }
